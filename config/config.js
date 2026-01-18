44bc807b5c419f136cabe28e8b22fa65ee3208a7
Ankit Kumar <ankitkumarm9546@gmail.com>
2026-01-18 12:26:39 +0530

    added .env

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  MONGODB_URI: 'mongodb+srv://ankitkumarm9546_db_user:aM365UOWmzMDphlc@cluster0.86m2zu9.mongodb.net/?appName=Cluster0',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://ankitkumarm9546_db_user:aM365UOWmzMDphlc@cluster0.86m2zu9.mongodb.net/?appName=Cluster0',
   PORT: 3000,
   JWT_SECRET: process.env.JWT_SECRET || 'food4u_secret',
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '7d'
